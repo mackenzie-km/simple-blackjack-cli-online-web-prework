e8d9fbff04480211eeb140c9f93d1c97f8fe768c
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-21 04:06:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,12 +58,12 @@ def runner
 welcome
 card_total = 0
 card_total = initial_round 
-binding.pry
 if card_total < 21
 until card_total >= 21
     card_total = hit?(card_total)
     return card_total
 end
+binding.pry
 else
 end_game(card_total)
 end
