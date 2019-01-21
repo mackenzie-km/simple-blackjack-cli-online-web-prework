a66cd7fddc4e1613f01b481deb657ad9aa584b7c
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-21 04:07:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,12 +59,13 @@ welcome
 card_total = 0
 card_total = initial_round 
 if card_total < 21
-until card_total >= 21
+  until card_total >= 21
     card_total = hit?(card_total)
+    binding.pry
     return card_total
 end
 else
-  binding.pry
-end_game(card_total)
+  end_game(card_total)
+end
 end
     
